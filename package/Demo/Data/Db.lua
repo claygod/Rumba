d121c5d1484c847f32151d4b68451858e6ba2099
claygod <claygod@yandex.ru>
2016-02-26 12:31:35 +0300

    The test site is added to the text 'Lorem ipsum dolor sit amet...'

--- package/Demo/Data/Db.lua
+++ package/Demo/Data/Db.lua
@@ -7,19 +7,19 @@ function genObj(class_name, conf, obj)
 
 local M = {
 	probe1 = {
-		title	= 'Title 1',
+		title	= 'Article 1',
 		description	= 'Brief description of the article 1',
-		content		= 'The text of article number 1. Bla-bla-bla, Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla.Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. <br> Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla..',
+		content		= 'The text of article number 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 	},
 	probe2 = {
-		title	= 'Title 2',
+		title	= 'Article 2',
 		description	= 'Brief description of the article 2',
-		content		= 'The text of article number 2. Bla-bla-bla, Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla.Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. <br> Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla..',
+		content		= 'The text of article number 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 	},
 	probe3 = {
-		title	= 'Title 3',
+		title	= 'Article 3',
 		description	= 'Brief description of the article 3',
-		content		= 'The text of article number 3. Bla-bla-bla, Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla.Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. <br> Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla. Bla-bla-bla, bla bla bla..',
+		content		= 'The text of article number 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 	},
 }
 
