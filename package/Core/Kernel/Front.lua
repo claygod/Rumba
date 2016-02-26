754472cb85ccb6d46d442640e66451257d50d874
claygod <claygod@yandex.ru>
2016-02-26 12:26:18 +0300

    Removed unnecessary comments

--- package/Core/Kernel/Front.lua
+++ package/Core/Kernel/Front.lua
@@ -21,21 +21,13 @@ local L = {
 --]===================]
 
 function M.doJob(r)
-	
-	--prnt_r(L.obj)
 	local inquiry = L.obj.request.doJob(r)
-	--prnt('<hr>') prnt_r(inquiry)
 	local route = L.obj.router.doJob(inquiry)
-	--prnt_r(route)
 	local controller, err = di.get(route['controller'])
 	local html = controller.doJob(route)
 	prnt(html.text)
 	return html.status
 end
-
---[===================[
-	Private methods
---]===================]
 
 return M	
 end
